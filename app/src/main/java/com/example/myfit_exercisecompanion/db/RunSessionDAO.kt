4859b34238a5e95caf333ccf923f2d99df124f40
ArQcane <isaacquankai@gmail.com>
2022-06-08 21:25:47 +0800

    Room setup

--- app/src/main/java/com/example/myfit_exercisecompanion/db/RunSessionDAO.kt
+++ app/src/main/java/com/example/myfit_exercisecompanion/db/RunSessionDAO.kt
@@ -7,14 +7,34 @@ import com.example.myfit_exercisecompanion.models.RunSession
 @Dao
 interface RunSessionDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun upsert(runSession: RunSession): Long
+    suspend fun insertRunSession(runSession: RunSession)
+
+    @Delete
+    fun delete(runSession: RunSession)
+
+    @Query("SELECT * FROM runsession ORDER BY timestamp DESC")
+    fun getAllRunSessionsSortedByDate(): LiveData<List<RunSession>>
+
+    @Query("SELECT * FROM runsession ORDER BY timeInMilis DESC")
+    fun getAllRunSessionsSortedByTimeInMilis(): LiveData<List<RunSession>>
+
+    @Query("SELECT * FROM runsession ORDER BY caloriesBurnt DESC")
+    fun getAllRunSessionsSortedByCaloriesBurnt(): LiveData<List<RunSession>>
+
+    @Query("SELECT * FROM runsession ORDER BY avgSpeedInKMH DESC")
+    fun getAllRunSessionsSortedByAvgSpeed(): LiveData<List<RunSession>>
+
+    @Query("SELECT * FROM runsession ORDER BY distanceInMeters DESC")
+    fun getAllRunSessionsSortedByDistance(): LiveData<List<RunSession>>
 
     @Query("SELECT * FROM runsession")
     fun getAllRunSessions(): LiveData<List<RunSession>>
 
-    @Query("SELECT * FROM runsession ORDER BY id DESC LIMIT 1")
+    @Query("SELECT * FROM runsession ORDER BY timestamp ASC LIMIT 1")
     fun getMostRecentRunSession(): LiveData<RunSession>
 
-    @Delete
-    fun delete(runSession: RunSession)
+    @Query("SELECT SUM(timeInMilis) FROM runsession")
+    fun getTotalTimeInMilis(): LiveData<Long>
+
+
 }
